fbe204707c5c169669572d5979208490bc4b4054
hieubun1 <119400657+hieubun1@users.noreply.github.com>
2024-12-09 03:15:25 +0700

    Đưa file

---
+++ 1/app/Http/Controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view("home", compact("posts"));
+    }
+}
---
+++ 1/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Created At</th>
+            <th>Updated At</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($posts as $post)
+        <tr>
+            <td>{{ $post->id }}</td>
+            <td>{{ $post->title }}</td>
+            <td>{{ $post->content }}</td>
+            <td>{{ $post->created_at }}</td>
+            <td>{{ $post->updated_at }}</td>
+        </tr>
+        @endforeach
+    </tbody>
